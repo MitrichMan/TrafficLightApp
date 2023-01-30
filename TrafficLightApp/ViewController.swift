cb51760911d4dcaf2d4738955e6ad46455c5d04d
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-01-30 21:44:35 +0800

    Work with logic

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.none
+    
+    private let lightOn: CGFloat = 1
+    private let lightOff: CGFloat = 0.3
+    
+    private enum CurrentLight {
+        case none, red, yellow, green
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
     }
-
-
+    
+    
+    
+    @IBAction func startButtonTapped() {
+        startButton.setTitle("Next", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            redLightView.alpha = lightOff
+            yellowLightView.alpha = lightOn
+            currentLight = .yellow
+        case .yellow:
+            yellowLightView.alpha = lightOff
+            greenLightView.alpha = lightOn
+            currentLight = .green
+        case .green:
+            greenLightView.alpha = lightOff
+            redLightView.alpha = lightOn
+            currentLight = .red
+        default:
+            redLightView.alpha = lightOn
+            currentLight = .red
+        }
+    }
+    
 }
 
