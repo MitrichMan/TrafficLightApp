bbfd6f4b49e438dce0cb9180f7bf4ee3a6cbacb2
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-01-31 19:07:00 +0800

    Controller work changes

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -7,12 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class  ViewController: UIViewController {
+    
     @IBOutlet var redLightView: UIView!
     @IBOutlet var yellowLightView: UIView!
     @IBOutlet var greenLightView: UIView!
-
     
     @IBOutlet var startButton: UIButton!
     
@@ -20,23 +19,23 @@ class ViewController: UIViewController {
     
     private let lightOn: CGFloat = 1
     private let lightOff: CGFloat = 0.3
-        
+    
     private enum CurrentLight {
         case none, red, yellow, green
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
- 
-    override func viewDidAppear(_ animated: Bool) {
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
         redLightView.layer.cornerRadius = redLightView.frame.width / 2
         yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
         greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
     }
     
-        @IBAction func startButtonTapped() {
-            
+    @IBAction func startButtonTapped() {
+        if startButton.currentTitle == "Start" {
+            startButton.setTitle("Next", for: .normal)
+        }
+        
         switch currentLight {
         case .red:
             redLightView.alpha = lightOff
@@ -51,11 +50,9 @@ class ViewController: UIViewController {
             redLightView.alpha = lightOn
             currentLight = .red
         default:
-            startButton.setTitle("Next", for: .normal)
             redLightView.alpha = lightOn
             currentLight = .red
         }
     }
-    
 }
 
